e50d2bb73a85c19da88c4ad9fa8035f037d4b780
timuryunusov88@gmail.com <timuryunusov88@gmail.com>
2025-05-22 20:04:57 -0500

    Applications endpoint added

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.app.api.endpoints import applications
+
 from .api.endpoints import companies, jobs
 
 app = FastAPI(title="Job Board API")
@@ -17,7 +19,8 @@ app.add_middleware(
 # Include routers
 app.include_router(companies.router, prefix="/companies", tags=["companies"])
 app.include_router(jobs.router, prefix="/job-postings", tags=["job-postings"])
+app.include_router(applications.router, prefix="/applications", tags=["applications"])
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Job Board API"} 
+    return {"message": "Welcome to the Job Board API designed and developed by the Timur at Job Board"} 
